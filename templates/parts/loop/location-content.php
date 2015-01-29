36e9a5bd9816eb414196c036898fad8cc2d4e938
Anthony Garand <anthonygarand@gmail.com>
2015-01-28 21:45:03 -0600

    Update map embed for location template

--- templates/parts/loop/location-content.php
+++ templates/parts/loop/location-content.php
@@ -12,32 +12,14 @@
 // $address, $show_directions_link, $directions_url, $phone, $times, $map_lat, $map_lng, $map_type, $map_zoom
 extract( ctfw_location_data() );
 
-$google_map = ctfw_google_map( array(
-    'latitude'  => $map_lat,
-    'longitude' => $map_lng,
-    'type'    => $map_type,
-    'zoom'    => $map_zoom
-  ) );
-
+$address = ctfw_location_data()['address'];
 ?>
-<?php if ( $google_map ) : ?>
-  <div class="rock-location-full-map">
-    <?php echo $google_map; ?>
-  </div>
-<?php endif; ?>
 
-<div class="entry-location-meta">
-
-  <?php if ( $directions_url ) : ?>
-    <div class="rock-location-full-direction">
-      <a href="<?php echo esc_url( $directions_url ); ?>" target="_blank" class="rock-button">
-        <i class="genericon genericon-location"></i>
-        <?php _ex( 'Get Directions', 'location', 'rock' ); ?>
-      </a>
-    </div>
-  <?php endif; ?>
-
+<?php if ( $address ) : ?>
+<div class="rock-event-full-map">
+  <iframe class="event-map" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=<?php echo urlencode( $address ); ?>&output=embed"></iframe>
 </div>
+<?php endif; ?>
 
 <div class="entry-content">
 	<?php the_content( __( 'Read More <span class="meta-nav">&rarr;</span>', 'rock' ) ); ?>
--- templates/parts/loop/location-title.php
+++ templates/parts/loop/location-title.php
@@ -13,35 +13,32 @@ $show_directions_link = ctfw_location_data()['show_directions_link'];
 $directions_url = ctfw_location_data()['directions_url'];
 $phone = ctfw_location_data()['phone'];
 $times = ctfw_location_data()['times'];
-$map_lat = ctfw_location_data()['map_lat'];
-$map_lng = ctfw_location_data()['map_lng'];
-$map_type = ctfw_location_data()['map_type'];
-$map_zoom = ctfw_location_data()['map_zoom'];
 ?>
 <header class="entry-header">
 	<div class="entry-header-row">
 		<div class="entry-header-column">
-			<?php if ( has_post_thumbnail() ) : ?>
-				<div class="rock-entry-image">
-					<?php rock_post_image(); ?>
-				</div>
-			<?php endif; ?>
 			<a href="<?php the_permalink(); ?>" rel="permalink"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
-			<div class="event-meta">
+			<div class="location-meta">
+				<?php if ( $address ) : ?>
 				<span>
 					<i class="genericon genericon-location"></i>
-					<?php echo nl2br( wptexturize( $address ) ); ?>
+					<?php echo wptexturize( $address ); ?>
 				</span>
+				<?php endif; ?>
+				<?php if ( $phone ) : ?>
 				<span>
 					<i class="genericon genericon-phone"></i>
 					<?php echo esc_html( $phone ); ?>
 				</span>
+				<?php endif; ?>
+				<?php if ( $times ) : ?>
 				<span>
 					<i class="genericon genericon-time"></i>
-					<?php echo nl2br( wptexturize( $times ) ); ?>
+					<?php echo wptexturize( $times ); ?>
 				</span>
+				<?php endif; ?>
 				</span>
-			</div><!-- .sermon-meta -->
+			</div><!-- .location-meta -->
 		</div><!-- .entry-header-column -->
 	</div><!-- .entry-header-row -->
 </header><!-- .entry-header -->
