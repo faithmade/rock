ac10935a6e842c8b165b8c7e4a8220941a19292d
Anthony Garand <garand@garand.me>
2015-07-11 21:55:27 -0400

    Add default widget placeholder

---
+++ default-widget.php
@@ -0,0 +1,5 @@
+<div class="widget widget_default">
+  <h6 class="widget-title">Custom Widget Area</h6>
+  <p>This is a custom widget area. To configure what displays here, go to the widgets section of the WordPress admin and add some widgets to the <strong><?php echo $sidebar_name; ?></strong>.</p>
+  <p><a href="<?php echo get_site_url() ?>/wp-admin/widgets.php" class="button">Configure Widgets</a></p>
+</div>
--- sidebar.php
+++ sidebar.php
@@ -4,14 +4,18 @@
  *
  * @package rock
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
 
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php
+    if ( is_active_sidebar( 'sidebar-1' ) ) {
+      dynamic_sidebar( 'sidebar-1' );
+    }
+    else {
+      $sidebar_name = "Left Sidebar";
+      include 'default-widget.php';
+    }
+  ?>
 
 </div><!-- #secondary -->
