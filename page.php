0eaf343f6e617bb78300ffdee59bfaef3fc62716
Matt Pritchett <matthew.pritchett@liftux.com>
2016-11-10 16:47:30 -0500

    Removing comments from pages.

--- page.php
+++ page.php
@@ -23,12 +23,6 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php if ( comments_open() || get_comments_number() ) : ?>
-
-				<?php comments_template(); ?>
-
-			<?php endif; ?>
-
 		<?php endwhile; ?>
 
 	</main><!-- #main -->
