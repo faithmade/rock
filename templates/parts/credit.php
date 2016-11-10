fbd35ecfcab96d68310d24b40402c26ee36adce4
Matt Pritchett <matthew.pritchett@liftux.com>
2016-11-10 12:03:54 -0500

    One more tweak to escaping footer credit.

--- templates/parts/credit.php
+++ templates/parts/credit.php
@@ -11,7 +11,7 @@
 <?php
 
 printf(
-	esc_html_x( 'Copyright %1$s %2$d %3$s. Powered by ', '1. copyright symbol, 2. year, 3. site title', 'rock' ) . '<a href="https://faithmade.com/">Faithmade</a>.',
+	esc_html_x( 'Copyright %1$s %2$d %3$s. Powered by ', '1. copyright symbol, 2. year, 3. site title', 'rock' ) . __( '<a href="https://faithmade.com/">Faithmade</a>.', 'rock' ),
 	'&copy;',
 	date( 'Y' ),
 	get_bloginfo( 'blogname' )
