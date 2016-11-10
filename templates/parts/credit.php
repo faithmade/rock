af541c2962c9b6eb1a277bbb35ecf15ed2193ac7
Matt Pritchett <matthew.pritchett@liftux.com>
2016-11-10 11:48:47 -0500

    Updating footer credit.

--- templates/parts/credit.php
+++ templates/parts/credit.php
@@ -11,40 +11,11 @@
 <?php
 
 printf(
-	esc_html_x( 'Copyright %1$s %2$d %3$s', '1. copyright symbol, 2. year, 3. site title', 'rock' ),
+	esc_html_x( 'Copyright %1$s %2$d %3$s. Powered by Faithmade.', '1. copyright symbol, 2. year, 3. site title', 'rock' ),
 	'&copy;',
 	date( 'Y' ),
 	get_bloginfo( 'blogname' )
 );
-
-/**
- * Filter the footer author credit display.
- *
- * @since 1.0.0
- *
- * @var bool
- */
-if ( (bool) apply_filters( 'rock_author_credit', true ) ) {
-
-	echo ' &mdash; ';
-
-	$theme = wp_get_theme();
-
-	printf(
-		esc_html_x( '%1$s theme by %2$s', '1. theme name link, 2. theme author link', 'rock' ),
-		sprintf(
-			'<a href="%s">%s</a>',
-			esc_url( $theme->get( 'ThemeURI' ) ),
-			esc_html( $theme->get( 'Name' ) )
-		),
-		sprintf(
-			'<a href="%s" rel="author">%s</a>',
-			esc_url( $theme->get( 'AuthorURI' ) ),
-			esc_html( $theme->get( 'Author' ) )
-		)
-	);
-
-}
 
 ?>
 </div>
